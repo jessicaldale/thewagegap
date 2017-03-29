fce09c6a0ca8552e525c890dce86cbb47e60bc40
Jess Dale <jessicaldale@gmail.com>
2017-03-29 14:10:28 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 window.onload = function () {   
   function calculateGap () {
-    var salary =
-    var math =
-    var gap =
-    gap = 
-    document.getElementById('gap1').innerHTML = "Black Women's Salary = $"+payment;
+    var salary = document.getElementByID('currentSalary').value;
+    var payrate = .75 //value
+    var math = (salary * 100) / payrate;
+    var gap = math.toFixed(2);
+    gap = gap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.getElementById('gap').innerHTML = "Black Women's Salary = $"+payment;
   }
 };
